6800284ea6a85f002ffc53098ad2eb9ade4115d2
tonousa <tonousa@hotmail.com>
2015-07-28 19:47:23 -0400

    3

--- UrlsAndRoutes/App_Start/RouteConfig.cs
+++ UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -40,6 +40,9 @@ namespace UrlsAndRoutes
 
             //myRoute.DataTokens["UseNamespaceFallback"] = false;
 
+            routes.MapRoute("DiskFile", "Content/StaticContent.html",
+                new { controller = "Customer", action = "List" });
+
             routes.MapRoute("ChromeRoute", "{*catchall}",
                 new { controller = "Home", action = "Index" },
                 new { customConstraint = new UserAgentConstraint("Chrome") },
