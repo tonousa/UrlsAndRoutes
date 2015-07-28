1c3a40e5387d3aa2a87cbcaceb8df9c1f705e01a
tonousa <tonousa@hotmail.com>
2015-07-28 16:11:24 -0400

    Start

---
+++ UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using UrlsAndRoutes.Infrastructure;
+
+namespace UrlsAndRoutes
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
+
+            //routes.MapRoute("AddControllersRoute", "Home/{action}/{id}/{*catchall}",
+            //    new
+            //    {
+            //        controller = "Home",
+            //        action = "Index",
+            //        id = UrlParameter.Optional
+            //    },
+            //    new[] { "UrlsAndRoutes.AdditionalControllers" });
+
+            Route myRoute = routes.MapRoute("MyRoute", "{controller}/{Action}/{id}/{*catchall}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                new {  
+                        customConstraint = new UserAgentConstraint("Chrome")
+                    }, 
+                new[] { "UrlsAndRoutes.AdditionalControllers" });
+
+            myRoute.DataTokens["UseNamespaceFallback"] = false;
+        }
+    }
+}
