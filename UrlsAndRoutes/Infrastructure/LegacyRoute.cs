9f88a3e3e6fab7db5b3ff6a18e30de5243a2c738
tonousa <tonousa@hotmail.com>
2015-07-30 09:55:22 -0400

    368

---
+++ UrlsAndRoutes/Infrastructure/LegacyRoute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace UrlsAndRoutes.Infrastructure
+{
+    public class LegacyRoute : RouteBase
+    {
+        private string[] urls;
+
+        public LegacyRoute(params string[] targetUrls)
+        {
+
+        }
+    }
+}
