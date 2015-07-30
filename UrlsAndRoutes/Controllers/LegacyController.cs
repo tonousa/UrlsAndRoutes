1de15f428280a96a733740b712ac9f87b9edf082
tonousa <tonousa@hotmail.com>
2015-07-30 00:32:00 -0400

    367

---
+++ UrlsAndRoutes/Controllers/LegacyController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UrlsAndRoutes.Controllers
+{
+    public class LegacyController : Controller
+    {
+        //
+        // GET: /Legacy/
+
+        public ActionResult GetLegacyUrl(string legacyUrl)
+        {
+            return View((object)legacyUrl);
+        }
+
+    }
+}
